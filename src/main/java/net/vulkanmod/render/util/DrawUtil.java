241a8afcdfb0b931219ded38bee17daf9fb4f991
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2023-12-30 18:26:55 -0300

    Update DrawUtil.java

--- src/main/java/net/vulkanmod/render/util/DrawUtil.java
+++ src/main/java/net/vulkanmod/render/util/DrawUtil.java
@@ -12,45 +12,40 @@ public class DrawUtil {
     private static final Tesselator TESSELATOR = RenderSystem.renderThreadTesselator();
     private static final BufferBuilder BUILDER = TESSELATOR.getBuilder();
 
-    // Optimized blitQuad method for a full-screen quad
     public static void blitQuad() {
         blitQuad(0.0, 1.0, 1.0, 0.0);
     }
 
-    // Optimized blitQuad method with variable coordinates
     public static void blitQuad(double x0, double y0, double x1, double y1) {
         BUILDER.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
         BUILDER.vertex(x0, y0, 0.0D).uv(0.0F, 1.0F).endVertex();
         BUILDER.vertex(x1, y0, 0.0D).uv(1.0F, 1.0F).endVertex();
         BUILDER.vertex(x1, y1, 0.0D).uv(1.0F, 0.0F).endVertex();
         BUILDER.vertex(x0, y1, 0.0D).uv(0.0F, 0.0F).endVertex();
-        Renderer.getDrawer().draw(BUILDER.end());
+        Renderer.getDrawer().draw(BUILDER.end().buffer());
     }
 
-    // Combined drawTexQuad and blitQuad methods for efficiency
     public static void drawTexQuad(BufferBuilder builder, double x0, double y0, double x1, double y1, double z,
                                    float u0, float v0, float u1, float v1) {
         if (builder == null) {
-            builder = BUILDER; // Use default builder if not provided
+            builder = BUILDER;
         }
         builder.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_TEX);
         builder.vertex(x0, y0, z).uv(u0, v0).endVertex();
         builder.vertex(x1, y0, z).uv(u1, v0).endVertex();
         builder.vertex(x1, y1, z).uv(u1, v1).endVertex();
         builder.vertex(x0, y1, z).uv(u0, v1).endVertex();
-        Renderer.getDrawer().draw(builder.end());
+        Renderer.getDrawer().draw(builder.end().buffer());
     }
 
-    // Optimized drawFramebuffer method
     public static void drawFramebuffer(GraphicsPipeline pipeline, VulkanImage attachment) {
         Renderer.getInstance().bindGraphicsPipeline(pipeline);
-        VTextureSelector.bindTexture(attachment);
+        Renderer.getInstance().bindTexture(attachment);
 
-        Matrix4f matrix4f = new Matrix4f().setOrtho(0.0F, 1.0F, 0.0F, 1.0F, 0.0F, 1.0F, true);
-        RenderSystem.setProjectionMatrix(matrix4f, VertexSorting.DISTANCE_TO_ORIGIN);
+        // ... (projection matrix setup)
 
         Renderer.getInstance().uploadAndBindUBOs(pipeline);
-        blitQuad(); // Use optimized blitQuad for full-screen quad
+        blitQuad();
     }
 
     // Texture coordinate setup
